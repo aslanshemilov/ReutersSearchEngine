d4883187d44ee6983ce48a1721a9030e0d7db8bd
shilpakancharla <shilpakancharla18@gmail.com>
2018-11-18 18:12:45 -0500

    javadoc for ContentRepository.java

--- h2o/src/main/java/com/h2o/content/ContentRepository.java
+++ h2o/src/main/java/com/h2o/content/ContentRepository.java
@@ -3,6 +3,13 @@ package com.h2o.content;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Query creation. Declare an interface, type it to domain class
+ * and ID type it will handle.
+ * 
+ * @author shilpakancharla
+ */
+
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Long>{
 
