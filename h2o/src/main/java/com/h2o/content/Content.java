51edc1b2e7b97746252f99d5a2c1dfdb923c7108
shilpakancharla <shilpakancharla18@gmail.com>
2018-11-18 18:00:48 -0500

    Javadoc for Content.java

--- h2o/src/main/java/com/h2o/content/Content.java
+++ h2o/src/main/java/com/h2o/content/Content.java
@@ -10,52 +10,97 @@ import io.swagger.annotations.ApiModelProperty;
 
 @Entity
 @ApiModel(description="All details about the Content. ")
+
 public class Content {
+
 	@Id
+
 	@GeneratedValue
+
+	/** ID (in Wrapper class) */
 	private Long id;
 	
-	@ApiModelProperty(notes="Name should have atleast 2 characters")
-	@Size(min=2, message="Name should have atleast 2 characters")
+	@ApiModelProperty(notes="Name should have at least 2 characters")
+	@Size(min=2, message="Name should have at least 2 characters")
+
+	/** Title */
 	private String title;
 	
+	/** Topic */
 	private String topics;
 	
-	public Content() {
-		super();
-	}
+	/**
+	 * Constructor for Content object 
+	 *
+	 * @param id
+	 * @param title
+	 * @param topics
+	 */
 
-	//public Content(Long id, String title, String date) {
-		public Content(Long id, String title, String topics) {		
+	public Content(Long id, String title, String topics) {		
 		super();
 		this.id = id;
 		this.title = title;
 		this.topics = topics;
 	}
 
-		public Long getId() {
-			return id;
-		}
+	/**
+	 * Getter method for ID.
+	 *
+	 * @return ID number of information/article
+	 */
 
-		public void setId(Long id) {
-			this.id = id;
-		}
+	public Long getId() {
+		return id;
+	}
 
-		public String getTopics() {
-			return topics;
-		}
+	/**
+	 * Setter method for ID.
+	 *
+	 * @param id ID number of information/article
+	 */
 
-		public void setTopics(String topics) {
-			this.topics = topics;
-		}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-		public String getTitle() {
-			return title;
-		}
+	/**
+	 * Getter method for topic.
+	 *
+	 * @return topics of information/article
+	 */
+	
+	public String getTopics() {
+		return topics;
+	}
 
-		public void setTitle(String title) {
-			this.title = title;
-		}
+	/**
+	 * Setter method for topics.
+	 *
+	 * @param topics of information/article
+	 */
 
-		
+	public void setTopics(String topics) {
+		this.topics = topics;
+	}
+
+	/**
+	 * Getter method for title.
+	 *
+	 * @return title of information/article
+	 */
+
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * Setter method for topics.
+	 *
+	 * @param topics of information/article
+	 */
+
+	public void setTitle(String title) {
+		this.title = title;
+	}		
 }
